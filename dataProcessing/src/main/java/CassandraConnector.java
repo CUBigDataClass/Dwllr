8da05d1872e0f8f39a4526737eb2310268d61f66
ryan-turner <ryan.turner@colorado.edu>
2018-03-10 09:36:45 -0700

    Initial setup that will enable us to query a cassandra database from Java using datastax

---
+++ dataProcessing/src/main/java/CassandraConnector.java
@@ -0,0 +1,10 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class CassandraConnector {
+    public static void main (String []args){
+        Cluster cluster;
+        Session session;
+
+    }
+}
