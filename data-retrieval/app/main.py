8b730e765499e691e6508df681d781f77869717d
Justin Cai <justincai.lps@gmail.com>
2018-04-16 14:50:41 -0600

    Added mappings.txt functionality (#18)
    * added error message function
    
    * added functionality to use mappings.txt

--- data-retrieval/app/main.py
+++ data-retrieval/app/main.py
@@ -1,50 +1,60 @@
 from flask import Flask, jsonify, request
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+from cassandra.query import dict_factory
 from flask_cors import CORS
 
-import time
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-cluster = Cluster(["cassandra"])
-session = cluster.connect("dwllr")  
+if os.environ.get("DWLLR_LOCAL", "false") == "true":
+    cluster = Cluster(["localhost"])
+else:
+    cluster = Cluster(["cassandra"])
+
+session = cluster.connect("dwllr")
+
+#output rows are dicts  
+session.row_factory = dict_factory
+
+#load mappings
+with open("mappings.txt", "r") as infile:
+    mappings = dict(map(lambda x: x.split(), infile))
+
+def error(msg):
+    return jsonify({
+        "error": {
+            "message": msg
+        }
+    })  
 
 @app.route("/api/search", methods=["GET"])
 def search():
-
-    city = request.args.get("city", -1)
-    if city == -1:
-        return jsonify({
-            "error": {
-                "message": "missing city zip codes"
-            }
-        })
+    city = request.args.get("city", None)
+    if not city:
+        return error("missing city zip codes")
 
     try:
         print(city)
+        s = "SELECT zip," + ",".join(mappings) + " FROM stats WHERE zip=%s"
         rv = session.execute(
-            SimpleStatement("SELECT zip, population FROM stats WHERE zip=%s"),
+            SimpleStatement(s),
             (str(city), )
         )
     except Exception as e:
-        return jsonify({
-            "error": {
-                "message": repr(e)
-            }
-        })
+        return error(repr(e))
 
     if len(rv.current_rows) == 0:
-        return jsonify({
-            "error": {
-                "message": "zip code not found"
-            }
-        })
+        return error("zip code not found")
+
+    row = rv.current_rows[0]
     return jsonify({
         "data": {
             "stats": {
-                "population": rv.current_rows[0].population
+                field_name: row[column_name] 
+                for column_name, field_name in mappings.items()
             }
         }
     })
