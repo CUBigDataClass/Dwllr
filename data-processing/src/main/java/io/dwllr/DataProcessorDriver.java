15308685071406d66c631e1ecade80c20736f6d9
ryan-turner <ryan.turner@colorado.edu>
2018-04-16 12:22:17 -0600

    Adding new data sets only requires update to the config file and the creation of the appropriate parsing and iterating classes

--- data-processing/src/main/java/io/dwllr/DataProcessorDriver.java
+++ data-processing/src/main/java/io/dwllr/DataProcessorDriver.java
@@ -1,128 +1,74 @@
 package io.dwllr;
 import io.dwllr.cassandra.Connect;
-import io.dwllr.processor.*;
 import io.dwllr.cassandra.Query;
-
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 public class DataProcessorDriver {
     public static void main(String[] args) {
-
+        int numberOfFiles = 5;
         Connect connection = new Connect();
-        //Will need to set these filepaths to wherever the file is located. Will include the file locations in the config when its created.
-        String zipCodeFile = "/datasets/us_postal_codes.csv";
-        String populationFile = "/datasets/population.csv";
-        String unemploymentFile = "/datasets/unemployment.csv";
-        String averageIncomeFile = "/datasets/averageIncome.csv";
+        String iter = "", parser = "", file = "", type = "";
+        for (int i = 1; i < numberOfFiles; i++) {
+            try {
+                FileInputStream fileInput = new FileInputStream("./resources/config.xml");
+                Properties properties = new Properties();
+                properties.loadFromXML(fileInput);
+                fileInput.close();
+                file = (String) properties.get("file" + i);
+                iter = (String) properties.get("DataRowIterator" + i);
+                parser = (String) properties.get("DataRowParser" + i);
+                type = (String) properties.get("DataType" + i);
 
-        //TODO: Set this up to work with config and java reflections. Code setup right now to establish steel thread.
-
-        //This is for adding the zip codes to the db
-        try{
-            CsvDataRowIterator iterator= new CsvDataRowIterator(zipCodeFile);
-            Optional<List<String>> row = iterator.getNextRow();
-            ZipDataRowParser zip = new ZipDataRowParser();
-            Query query;
-            while(row.isPresent()){
-                query = zip.getQueryFromData(row.get());
-                try{
-                    String finalQuery = query.getQuery();
-                    connection.query(finalQuery);
-                } catch (Exception e){
-                    System.out.println("QUERY FAILED");
-                }
-                row = iterator.getNextRow();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            System.out.println("Error creating iterator with filename");
-        }
+            try {
+                Class<?> classIterator = Class.forName("io.dwllr.processor." + iter);
+                Constructor<?> constructor = classIterator.getConstructor(String.class);
+                Object obj = constructor.newInstance(file);
+                Method methodCall = classIterator.getDeclaredMethod("getNextRow");
+                Object value = methodCall.invoke(obj);
+                Optional<List<String>> row = (Optional<List<String>>) value;
+                Class<?> parserClass = Class.forName("io.dwllr.processor." + parser);
+                Constructor<?> constructorParser = parserClass.getConstructor();
+                Object parserObj = constructorParser.newInstance();
+                Query query;
+                while (row.isPresent()) {
+                    Method methodCallQuery = parserClass.getDeclaredMethod("getQueryFromData", List.class);
+                    Object queryValue = methodCallQuery.invoke(parserObj, row.get());
+                    query = (Query) queryValue;
 
-
-        //This is for adding the population to the db
-        try{
-            CsvDataRowIterator iterator= new CsvDataRowIterator(populationFile);
-            Optional<List<String>> row = iterator.getNextRow();
-            PopulationDataRowParser pop = new PopulationDataRowParser();
-            Query query;
-            while(row.isPresent()){
-                query = pop.getQueryFromData(row.get());
-                try{
-                    String finalQuery = query.getFinalQuery();
-                    connection.query(finalQuery);
-                } catch (Exception e){
-                    System.out.println("QUERY FAILED");
+                    try {
+                        String finalQuery;
+                        if (type.equals("zip")) {
+                            finalQuery = query.getQuery();
+                        } else {
+                            finalQuery = query.getFinalQuery();
+                        }
+                        System.out.println(finalQuery);
+                        connection.query(finalQuery);
+                    } catch (Exception e) {
+                        System.out.println("QUERY FAILED");
+                    }
+                    value = methodCall.invoke(obj);
+                    row = (Optional<List<String>>) value;
                 }
-                row = iterator.getNextRow();
-            }
-        } catch (Exception e) {
-            System.out.println("Error creating iterator with filename");
-        }
-
-        //This is for adding the unemployment to the db
-        try{
-            CsvDataRowIterator iterator= new CsvDataRowIterator(unemploymentFile);
-            Optional<List<String>> row = iterator.getNextRow();
-            UnemploymentDataRowParser pop = new UnemploymentDataRowParser();
-            Query query;
-            while(row.isPresent()){
-                query = pop.getQueryFromData(row.get());
-                try{
-                    String finalQuery = query.getFinalQuery();
-                    connection.query(finalQuery);
-                } catch (Exception e){
-                    System.out.println("QUERY FAILED");
+                if (!row.isPresent()) {
+                    Method methodCallClose = classIterator.getDeclaredMethod("closeDataset");
+                    methodCallClose.invoke(obj);
+                    System.out.println("Closed and done");
                 }
-                row = iterator.getNextRow();
+
+            } catch (Exception e) {
+                System.out.println("Error creating iterator with filename");
             }
-        } catch (Exception e) {
-            System.out.println("Error creating iterator with filename");
         }
-
-        try{
-            CsvDataRowIterator iterator= new CsvDataRowIterator(averageIncomeFile);
-            Optional<List<String>> row = iterator.getNextRow();
-            AverageIncomeDataRowParser pop = new AverageIncomeDataRowParser();
-            Query query;
-            while(row.isPresent()){
-                query = pop.getQueryFromData(row.get());
-                try{
-                    String finalQuery = query.getFinalQuery();
-                    connection.query(finalQuery);
-                } catch (Exception e){
-                    System.out.println("QUERY FAILED");
-                }
-                row = iterator.getNextRow();
-            }
-        } catch (Exception e) {
-            System.out.println("Error creating iterator with filename");
-        }
-        connection.close();
-        /*
-        Driver pseudocode:
-
-        for each dataset:
-            create a new data row iterator
-
-            while get next row isn't empty:
-                get query from data row parser
-                buffer query
-                if query buffer reaches buffer limit (we decide this value, some constant)
-                    send to query manager to run in cassandra
-            close iterator
-
-        I think we can get the dataset list from some kind of config file. Then use java reflection features to spawn the correct classes
-        Ex:
-
-        datasets:
-            zips:
-                dataRowIterator: CsvDataRowIterator
-                dataRowParser: ZipDataRowParser
-                file: all-us-zip-codes.csv
-            population:
-                dataRowIterator: CsvDataRowIterator
-                dataRowParser: PopulationDataRowParser
-                file: population.csv
-         */
     }
 }
