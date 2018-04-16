d7d2151aeee717a676f974826aa897c943ef6028
ryan-turner <ryan.turner@colorado.edu>
2018-04-16 13:20:42 -0600

    Got rid of extraneous folder

--- data-processing/src/main/java/io/dwllr/cassandra/Connect.java
+++ data-processing/src/main/java/io/dwllr/cassandra/Connect.java
@@ -8,7 +8,7 @@ http://www.baeldung.com/cassandra-with-java
 public class Connect {
     static final String KEYSPACE = "dwllr";
     static final String TABLE = "data";
-    static final String IP = "cassandra";
+    static final String IP = "127.0.0.1";
     Cluster cluster = Cluster.builder().addContactPoint(IP).build();
     Session session = cluster.connect(KEYSPACE);
 
