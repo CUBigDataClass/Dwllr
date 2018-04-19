217dd6321f8f07399ea9db5e34c9ba8b6a248fa1
ryan-turner <ryan.turner@colorado.edu>
2018-04-19 13:44:34 -0600

    Simplified age and gender parser

--- data-processing/src/main/java/io/dwllr/processor/AgeAndGenderDataRowParser.java
+++ data-processing/src/main/java/io/dwllr/processor/AgeAndGenderDataRowParser.java
@@ -13,70 +13,29 @@ public class AgeAndGenderDataRowParser implements DataRowParser {
         String zipCode = dataRow.get(2);
         int space = zipCode.indexOf(' ');
         zipCode = zipCode.substring(space+1);
-
-        String totalPopulation = dataRow.get(3);
-        double population = Double.parseDouble(totalPopulation);
-
-        String malePopulation = dataRow.get(4);
-        double male = (Double.parseDouble(malePopulation)) / population;
-
-        String femalePopulation = dataRow.get(5);
-        double female = (Double.parseDouble(femalePopulation)) / population;
+        double population = Double.parseDouble(dataRow.get(3));
+        double male = (Double.parseDouble(dataRow.get(4))) / population;
+        double female = (Double.parseDouble(dataRow.get(5))) / population;
 
         //Age
-        String underFiveString = dataRow.get(6);
-        double underFive = (Double.parseDouble(underFiveString)) / population;
-
-        String fiveToTenString = dataRow.get(9);
-        double fiveToTen = (Double.parseDouble(fiveToTenString)) / population;
-
-        String tenToFourteenString = dataRow.get(12);
-        double tenToFourteen = (Double.parseDouble(tenToFourteenString)) / population;
-
-        String fiveteenToNineteenString = dataRow.get(15);
-        double fiveteenToNineteen = (Double.parseDouble(fiveteenToNineteenString)) / population;
-
-        String twentyToTwentyfourString = dataRow.get(18);
-        double twentyToTwentyfour = (Double.parseDouble(twentyToTwentyfourString)) / population;
-
-        String twentyFiveToTwentynineString = dataRow.get(21);
-        double twentyFiveToTwentynine = (Double.parseDouble(twentyFiveToTwentynineString)) / population;
-
-        String thirtyToThirtyfourString = dataRow.get(24);
-        double thirtyToThirtyfour = (Double.parseDouble(thirtyToThirtyfourString)) / population;
-
-        String thirtyfiveToThirtynineString = dataRow.get(27);
-        double thirtyfiveToThirtynine = (Double.parseDouble(thirtyfiveToThirtynineString)) / population;
-
-        String fourtyToFourtyfourString = dataRow.get(30);
-        double fourtyToFourtyfour = (Double.parseDouble(fourtyToFourtyfourString)) / population;
-
-        String fourtyfiveToFourtynineString = dataRow.get(33);
-        double fourtyfiveToFourtynine = (Double.parseDouble(fourtyfiveToFourtynineString)) / population;
-
-        String fiftyToFiftyfourString = dataRow.get(36);
-        double fiftyToFiftyfour = (Double.parseDouble(fiftyToFiftyfourString)) / population;
-
-        String fiftyfiveToFiftynineString = dataRow.get(39);
-        double fiftyfiveToFiftynine = (Double.parseDouble(fiftyfiveToFiftynineString)) / population;
-
-        String sixtyToSixtyfourString = dataRow.get(42);
-        double sixtyToSixtyfour = (Double.parseDouble(sixtyToSixtyfourString)) / population;
-
-        String sixtyFiveToSixtynineString = dataRow.get(45);
-        double sixtyFiveToSixtyNine = (Double.parseDouble(sixtyFiveToSixtynineString)) / population;
-
-        String seventyToSeventyfourString = dataRow.get(48);
-        double seventyToSeventyfour = (Double.parseDouble(seventyToSeventyfourString)) / population;
-
-        String seventyfiveToSeventynineString = dataRow.get(51);
-        double seventyfiveToSeventynine = (Double.parseDouble(seventyfiveToSeventynineString)) / population;
-
-        String eightyToEightyfourString = dataRow.get(54);
-        double eightyToEightyfour = (Double.parseDouble(eightyToEightyfourString)) / population;
-
-        String eightyFiveAndOverString = dataRow.get(57);
-        double eightyFiveAndOver = (Double.parseDouble(eightyFiveAndOverString)) / population;
+        double underFive = (Double.parseDouble(dataRow.get(6))) / population;
+        double fiveToTen = (Double.parseDouble(dataRow.get(9))) / population;
+        double tenToFourteen = (Double.parseDouble(dataRow.get(12))) / population;
+        double fiveteenToNineteen = (Double.parseDouble(dataRow.get(15))) / population;
+        double twentyToTwentyfour = (Double.parseDouble(dataRow.get(18))) / population;
+        double twentyFiveToTwentynine = (Double.parseDouble(dataRow.get(21))) / population;
+        double thirtyToThirtyfour = (Double.parseDouble(dataRow.get(24))) / population;
+        double thirtyfiveToThirtynine = (Double.parseDouble(dataRow.get(27))) / population;
+        double fourtyToFourtyfour = (Double.parseDouble(dataRow.get(30))) / population;
+        double fourtyfiveToFourtynine = (Double.parseDouble(dataRow.get(33))) / population;
+        double fiftyToFiftyfour = (Double.parseDouble(dataRow.get(36))) / population;
+        double fiftyfiveToFiftynine = (Double.parseDouble(dataRow.get(39))) / population;
+        double sixtyToSixtyfour = (Double.parseDouble(dataRow.get(42))) / population;
+        double sixtyFiveToSixtyNine = (Double.parseDouble(dataRow.get(45))) / population;
+        double seventyToSeventyfour = (Double.parseDouble(dataRow.get(48))) / population;
+        double seventyfiveToSeventynine = (Double.parseDouble(dataRow.get(51))) / population;
+        double eightyToEightyfour = (Double.parseDouble(dataRow.get(54))) / population;
+        double eightyFiveAndOver = (Double.parseDouble(dataRow.get(57))) / population;
 
         Double lessThanTen = fiveToTen + underFive;
         Double tenToNineteen = tenToFourteen + fiveteenToNineteen;
