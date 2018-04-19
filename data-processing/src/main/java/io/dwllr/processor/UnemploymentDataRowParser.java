19809297b6eb0284463ca492ee514454e62566df
ryan-turner <ryan.turner@colorado.edu>
2018-04-19 09:38:03 -0600

    Parse out percent symbol from query data

--- data-processing/src/main/java/io/dwllr/processor/UnemploymentDataRowParser.java
+++ data-processing/src/main/java/io/dwllr/processor/UnemploymentDataRowParser.java
@@ -8,7 +8,9 @@ public class UnemploymentDataRowParser implements DataRowParser {
 
     @Override
     public Query getQueryFromData(List<String> dataRow) {
-        String queryString = "UPDATE stats SET unemployment = '" + dataRow.get(1) + "'"
+        String unemployment = dataRow.get(1);
+        unemployment = unemployment.substring(0, unemployment.length()-1);
+        String queryString = "UPDATE stats SET unemployment = '" + unemployment + "'"
                 + " WHERE zip = '" + dataRow.get(0) + "';";
 
         // Wrap queryString into Query here
