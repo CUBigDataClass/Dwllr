45fba35f866904f0af3a410a746996beacbd6f62
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-04-26 13:48:50 -0600

    React -> Vue

---
+++ frontend/dwllr/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    sup
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+#app {
+
+}
+</style>
