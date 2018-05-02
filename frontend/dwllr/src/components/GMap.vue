5af33f686bf5d13449aef28eee5983118a282061
Ryan Rouleau <ryan@ryanrouleau.com>
2018-05-02 10:20:59 -0600

    implement search by attribute on front end(#29)

--- frontend/dwllr/src/components/GMap.vue
+++ frontend/dwllr/src/components/GMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="g-map">
-    <gmap-map style="width:100%; height:100%;" :zoom="zoom" :center="{lat:this.coords.lat+2, lng:this.coords.lng+1.6}" ref="map">
+    <gmap-map style="width:100%; height:100%;" :zoom="zoom" :center="{lat:this.coords.lat+1, lng:this.coords.lng+0.6}" ref="map">
       <gmap-marker
         v-for="(m, index) in markers"
         :key="index"
@@ -25,6 +25,11 @@ export default {
 
   mounted () {
     Bus.$on('search_submitted', this.updateMarker);
+    Bus.$on('close_search_attr_results', () => {
+      //this.center = "{lat: 40.0902, lng: -98.7129}";
+      this.$refs.map.panTo({lat: 40.0902, lng: -98.7129});
+      this.zoom = 5;
+    });
     this.$refs.map.panTo({lat:this.coords.lat+2, lng:this.coords.lng+1.6});
 
   },
@@ -37,7 +42,7 @@ export default {
       //this.coords = place.coords;
       this.zoom = 6.2;
       this.markers = [place.coords];
-      this.$refs.map.panTo({lat:place.coords.lat+2, lng:place.coords.lng+1.6});
+      this.$refs.map.panTo({lat:place.coords.lat-0.5, lng:place.coords.lng+0.8});
     }
 
   },
--- frontend/dwllr/src/components/result-cards/SimilarCities.vue
+++ frontend/dwllr/src/components/result-cards/SimilarCities.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="similar-cities">
-    <similar-city v-for="city in cities" :key="city" :zip="city"></similar-city>
+    <span @click="unActivateResults">
+      <similar-city v-for="city in cities" :key="city" :zip="city"></similar-city>
+    </span>
   </div>
 </template>
 
 <script>
 import SimilarCity from './SimilarCity.vue';
+import Bus from '../bus.js';
 
 import axios from 'axios';
 
@@ -16,7 +19,14 @@ export default {
 
   components: {
     SimilarCity,
+    Bus,
     axios
+  },
+
+  methods: {
+    unActivateResults () {
+      Bus.$emit('unactivate-results');
+    }
   },
 
   watch: {
