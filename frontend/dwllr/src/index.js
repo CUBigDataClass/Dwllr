3a5327ccc0fc7da227a281ddb253c4a702e7cdf8
Ryan Rouleau <ryan@ryanrouleau.com>
2018-08-06 11:16:03 -0600

    add react router

--- frontend/dwllr/src/index.js
+++ frontend/dwllr/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { render } from 'react-dom';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import Root from './components/Root';
 import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+render(
+  <Root store={store} />,
   document.getElementById('root')
 );
 
