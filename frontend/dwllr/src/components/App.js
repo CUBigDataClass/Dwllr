0c873cc4db06ca649dd4beec4e8bb0e84a73aa09
Ryan Rouleau <ryan@ryanrouleau.com>
2018-08-10 14:47:03 -0600

    add yeti

--- frontend/dwllr/src/components/App.js
+++ frontend/dwllr/src/components/App.js
@@ -15,8 +15,32 @@ class App extends Component {
         <Route exact path="/" component={Divider} />
         <Route exact path="/results" component={CityResultContainer} />
         <Route path="/results/attribute" component={AttributeSearchResultContainer}/>
+        <Route exact path="/results" component={Yetti} />
       </div>
     );
+  }
+}
+
+class Yetti extends React.Component {
+  
+  state = {
+    hide: true,
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({hide: false})
+    }, 1500);
+  }
+  
+  render() {
+    return (
+      <img 
+        src="https://i.imgur.com/y4wcil5.png" 
+        style={{ position: 'absolute', zIndex:11, left:435, top:118, width: '30px', height: '30px', transition: '0.5s ease' }}
+        className={this.state.hide ? 'hide-yetti' : 'show-yetti'}
+      />
+    )
   }
 }
 
